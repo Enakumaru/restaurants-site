fb6811f85b907b184f26caca1c00c087f6838c91
Rishakumar62 <36103646+Rishakumar62@users.noreply.github.com>
2022-01-17 16:11:55 +0530

    depy-4

--- cofesite/demo/settings.py
+++ cofesite/demo/settings.py
@@ -4,10 +4,10 @@ import django_heroku
 from decouple import config 
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
-DEBUG = False
+DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-ALLOWED_HOSTS = ['http://127.0.0.1:8000/','https://rishi-restaurant.herokuapp.com/']
+ALLOWED_HOSTS = ['https://rishi-restaurant.herokuapp.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
